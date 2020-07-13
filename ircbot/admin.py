e7ce1d54e2c351dde69360e90d09b6cc4f70a47e
chang <chungsean74@gmail.com>
2020-07-13 15:06:01 +0800

    init commit

--- ircbot/admin.py
+++ ircbot/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from ircbot.models import users
+from ircbot.models import registerform
 
 class usersAdmin(admin.ModelAdmin):
     list_display=('uid','question')
+    
+class registerformAdmin(admin.ModelAdmin):
+     list_display = ('cid', 'name', 'phone', 'email', 'fbname', 'lineid', 'willing', 'firsttime')
+     ordering = ('-willing',)
+     
 admin.site.register(users, usersAdmin)
+admin.site.register(registerform, registerformAdmin)
--- ircbot/models.py
+++ ircbot/models.py
@@ -9,12 +9,12 @@ class users(models.Model):
 class registerform(models.Model):
     cid = models.CharField(max_length=50, null=False)
     name = models.CharField(max_length=10, null=False)
-    phone = models.CharField(max_length=15, null=False)
-    email = models.CharField(max_length=30, null=False)
-    fbname = models.CharField(max_length=30, null=False)
-    lineid = models.CharField(max_length=30, null=False)
+    phone = models.CharField(max_length=15, blank=True, default='')
+    email = models.EmailField(max_length=30, blank=True, default='')
+    fbname = models.CharField(max_length=30, blank=True, default='')
+    lineid = models.CharField(max_length=30, blank=True, default='')
     willing = models.CharField(max_length=5, null=False)
     firsttime = models.CharField(max_length=5, null=False)
     
     def __str__(self):
-        return self.id
+        return self.cid
