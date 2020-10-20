fee6df1021b17627dc313204ff93fb08e28bdb0e
Seanchang <chungsean74@gmail.com>
2020-10-20 09:53:00 +0800

    init commit

--- module/func.py
+++ module/func.py
@@ -14,12 +14,20 @@ endpoint_key = "8aa14541-c826-43de-8585-d1e388b4f96e"
 kb = "ba477641-32e6-4681-8385-c1c84c52c445"
 method = "/qnamaker/knowledgebases/" + kb + "/generateAnswer"'''
 
-def sendUse(event):  #@使用說明
+def sendCourse(event):  #@使用說明
     try:
-        text1 ="對於初次接觸的使用者，可以透過下方的圖文選單，點選你想要使用的功能\n如果有其他問題想要諮詢的話，可以私訊社團臉書粉專，我們會盡快為你解惑!"
-        message = TextSendMessage(
+        text1 ="以下為我們這學期的課程規劃:"
+        text2 ="基礎班\n1.認識Python\n2.邏輯與迴圈\n3.串列與字典\n4.函式與例外處理\n5.檔案的寫入與讀取"
+        text3 ="進階班\n1.網頁基礎知識介紹\n2.擷取JSON/HTML格式資料\n3.發送API請求\n4.Numpy&Pandas介紹\n5.資料視覺化\n6.瀏覽器自動化"
+        message = [TextSendMessage(
             text = text1
         )
+        TextSendMessage(
+            text = text2
+        )
+        TextSendMessage(
+            text = text3
+        )]
         line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='系統好像有點問題，請再試一次!'))
@@ -34,7 +42,7 @@ def sendData(event, user_id):
                     title='填寫正式入社表單',
                     text='歡迎加入國北資研的大家庭!',
                     actions=[
-                        URITemplateAction(label='前往填寫', uri='https://liff.line.me/1654433071-DPGqaOv2')
+                        URITemplateAction(label='前往填寫', uri='https://liff.line.me/1654949355-0OZEoq6d')
                     ]
                 )
             )
@@ -44,14 +52,34 @@ def sendData(event, user_id):
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='資料處理發生錯誤！'))
 
+def sendLeave(event, user_id):
+    try:
+        '''if not (registerform.objects.filter(cid=user_id).exists()):'''
+            message = TemplateSendMessage(
+                alt_text = '填寫社團請假表單',
+                template = ButtonsTemplate(
+                    thumbnail_image_url='https://i.imgur.com/CBilkHy.png',
+                    title='填寫社團請假表單',
+                    text='請假的同學一定要記得補課哦!',
+                    actions=[
+                        URITemplateAction(label='前往填寫', uri='https://liff.line.me/1654949349-byenoVgg')
+                    ]
+                )
+            )
+        '''else:
+            message = TextSendMessage(text = '你已經填寫過表單囉!')'''
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='資料處理發生錯誤！'))
+        
 def manageForm(event, mtext, user_id):
     try:
         flist = mtext[3:].split('/')
         cname = flist[0]
-        cgender = flist[1]
-        cbirth = flist[2]
+        csex = flist[1]
+        cstudentid = flist[2]
         cdepartment = flist[3]
-        cstudentid = flist[4]
+        cbirth = flist[4]
         cfbname = flist[5]
         clineid = flist[6]
         cfirsttime = flist[7]
@@ -61,12 +89,12 @@ def manageForm(event, mtext, user_id):
         text1 = "我們已收到你的回覆，資料如下:"
         text1 +="\n 姓名:" + cname
         text1 +="\n 學號:" + cstudentid
-        text1 +="\n 性別:" + cgender
+        text1 +="\n 性別:" + csex
         text1 +="\n 系級:" + cdepartment
         text1 +="\n 生日:" + cbirth
         text1 +="\n FB名稱:" + cfbname
         text1 +="\n LINE ID:" + clineid
-        text1 +="\n 未來欲參加的班級:" + cfirsttime
+        text1 +="\n 社團班級選擇:" + cfirsttime
         message = TextSendMessage(  #顯示表單資料
                     text = text1
                 )
@@ -74,7 +102,42 @@ def manageForm(event, mtext, user_id):
         line_bot_api.push_message(admin_uid, message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='資料處理發生錯誤！'))
-        
+
+def manageForm2(event, mtext, user_id):
+    try:
+        flist = mtext[3:].split('/')
+        cname = flist[0]
+        cstudentid = flist[1]
+        cdate = flist[2]
+        ctype = flist[3]
+        creason = flist[4]
+        '''unit = registerform.objects.create(cid=user_id, name=cname, studentid=cstudentid, gender=cgender, 
+        fbname=cfbname, lineid=clineid, department=cdepartment, birth=cbirth, firsttime=cfirsttime)
+        unit.save()'''
+        text1 = "我們已收到你的回覆，資料如下:"
+        text1 +="\n 姓名:" + cname
+        text1 +="\n 學號:" + cstudentid
+        text1 +="\n 請假日期:" + cdate
+        text1 +="\n 請假類別:" + ctype
+        text1 +="\n 請假事由:" + creason
+        message = TextSendMessage(  #顯示表單資料
+                    text = text1
+                )
+        line_bot_api.reply_message(event.reply_token,message)
+        line_bot_api.push_message(admin_uid, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='資料處理發生錯誤！'))
+
+def sendWait(event):
+    try:
+        text1 = "現在聊天機器人沒有客服功能，如有問題歡迎私訊臉書粉專"
+        message = TextSendMessage(
+            text = text1
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='系統好像有點問題，請再試一次!'))
+       
 '''def sendCancel(event, user_id):
     try:
         if registerform.objects.filter(cid=user_id).exists():
